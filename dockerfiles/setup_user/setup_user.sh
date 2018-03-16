b060b850ebee603ad68551f3e5e2d39f33e46f14
Bryan White <bryanchriswhite@gmail.com>
2018-03-16 05:08:48 -0400

    more devops

--- dockerfiles/setup_user/setup_user.sh
+++ dockerfiles/setup_user/setup_user.sh
@@ -13,8 +13,8 @@ service mongod start
 pm2 start -n bridge $root_dir/bin/storj-bridge -- -c $root_dir/config/storj-bridge/config.json -d $root_dir/config/storj-bridge
 
 #wait for things to come up
-echo "Sleeping for 3..."
-sleep 3
+echo "Sleeping for 5..."
+sleep 5
 
 ./create_user.sh
 ./import_keys.sh
